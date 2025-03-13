d6e451f305fd77bee929d89c751a37e082d2a6d4
byui_student1045 <sco22022@byui.edu>
2025-03-12 22:45:54 -0600

    Updated main to save memory and clear memory when not being used.

--- main.cpp
+++ main.cpp
@@ -4,10 +4,15 @@
 // Added these to use thread sleeping to create better UI.
 #include <chrono>
 #include <thread>
+// Include this library to use lists.
+#include <list>
 // Include the class into the main file.
 #include "calc.cpp"
 
 using namespace std;
+
+// Create a global varible to hold the list of objects in the heap. (This list must be kept at at a max of 5 objects inside.)
+std::list<Calc*> calc_list;
 
 // Test if number function.
 bool isNumb(const string testNum)
@@ -66,6 +71,121 @@ bool isZero (const string textNum)
     }
 }
 
+// Function that obtains an object from the heap.
+
+// Function that dynamically deletes an object from the heap.
+void deleteOnePointer(int index) 
+{
+    // Delete the obj at it's index.
+    // To do this create a varible to count as we cycle through the list.
+    int count = 0;
+
+    // Now loop through the list.
+    for (Calc *obj : calc_list) {
+        // Look to see if we are at the index.
+        if (count == index) {
+            // Remove from the list.
+            calc_list.remove(obj);
+            // Delete the pointer.
+            delete obj;
+            obj = nullptr;
+            // Now break out of the loop.
+            break;
+        }
+        // Add one to our counter.
+        count += 1;
+    }
+    // Return back to main function.
+    return;
+}
+
+// Function that dynamically deletes all objects from the heap on close.
+void deletePointers()
+{
+    // Check to see if it is empty.
+    if (!calc_list.empty()) {
+        // Loop through the list and delete all the pointers to free up memory.
+        for (Calc *obj : calc_list) {
+            // Delete the object to free up memory.
+            delete obj;
+            obj = nullptr; // Good practice to reset the pointer to avoid dangling pointers.
+        }
+        // Clear the list and return.
+        calc_list.clear();
+        // Return back to the previous function.
+        return;
+    } else {
+        // Just return.
+        return;
+    }
+}
+
+// Create secret menu to handle calculator memory using the heap.
+void secretMenu() 
+{
+    // This menu does not return anything it just does stuff.
+    // Check to see if the list is empty if it is display a message and return to the original function.
+    if (calc_list.empty())
+    {
+        std::cout << "You currently have no memory. :(\nPlease go and do some problems and come back after you are done." << std::endl;
+        std::cout << "" << std::endl;
+        // Create a return to the past function on hit of enter.
+        std::cout << "Press 'enter' to continue: ";
+        // Call this to clear up the inputs being accepted into the std bin.(If this is not cleared this will just move on because we already accepted an input before.)
+        std::cin.ignore();
+        // Use this to pause to continue on.
+        std::cin.get();
+        // Return.
+        return;
+    } else {
+        // Welcome the user.
+        std::cout << "Welcome to memory! Here are the last 5 problems you have done so far:" << std::endl;
+        // Create a variable to display the position of each memory object.
+        int mem = 0;
+        // Display all the objects here using a for each loop.
+        for (Calc *obj : calc_list) {
+            // Set mem plus 1 for each object in the list.
+            mem += 1;
+            // Now display the object by it's problem type.
+            if (obj->getProblemType() == 1) {
+                // Display it.
+                std::cout << "[" << mem << "] Addition" << std::endl;
+            } else if (obj->getProblemType() == 2) {
+                std::cout << "[" << mem << "] Subtraction" << std::endl; 
+            } else if (obj->getProblemType() == 3) {
+                std::cout << "[" << mem << "] Multiplication" << std::endl;
+            } else {
+                std::cout << "[" << mem << "] Division" << std::endl;
+            }
+
+        }
+
+        // Create one more to allow them to go back if they would like.
+        std::cout << "[6] Go Back" << std::endl;
+        
+        // Create a loop to make sure they select a response.
+        bool loopStop = false;
+        // Ask them to select one they would like to use.
+        while (loopStop = false) {
+            std::string userInput;
+            std::cout << "" << std::endl;
+            std::cout << "Please select which one you would like to view: ";
+            std::cin >> userInput;
+
+            // Check the input to move on.
+            if (isNumb(userInput)) {
+                // Go on to the next check.
+                loopStop = true;
+            } else {
+                // Error message, and then set loopStop to false.
+                std::cout << "Input is not a number please try again." << std::endl;
+                loopStop = false;
+            }
+        }
+        
+    }
+}
+
 // Create the object and return the result.
 int problemSolution(int numOne, int numTwo, int problemType) 
 {
@@ -78,15 +198,27 @@ int problemSolution(int numOne, int numTwo, int problemType)
     // Now save the solution to a variable to return to the main program.
     int solution = calc_pointer->getSolution(); // The arrow way to run these commands.
 
-    // Now delete it to free space on the heep and prevent memory leaks.
-    delete calc_pointer;
-    calc_pointer = nullptr; // Good practice to reset the pointer to avoid dangling pointers.
+    if (calc_list.empty()) {
+    // Add the new pointer to the back of the list.
+    // IT IS VERY IMPORTANT TO MAKE SURE THAT THIS IS TAKEN CARE OF WITH A DELETE TO PREVENT MEMORY LEAKS.
+    calc_list.push_back(calc_pointer);
+    } else {
+        // Check to see if the list has 5 items in it already.
+        if (calc_list.size() == 5) {
+            // Delete the first index, and then move on.
+            deleteOnePointer(0);
+            // Now add the new pointer.
+            calc_list.push_back(calc_pointer);
+        } else {
+            // Add the new pointer to the back of the list.
+            // IT IS VERY IMPORTANT TO MAKE SURE THAT THIS IS TAKEN CARE OF WITH A DELETE TO PREVENT MEMORY LEAKS.
+            calc_list.push_back(calc_pointer);
+        }
+    }
 
     // Return the solution to the main function.
     return solution;
 }
-
-// Create secret menu.
 
 // Main function being ran.
 int main()
@@ -105,23 +237,27 @@ int main()
         std::cout << "[2] Subtract" << std::endl;
         std::cout << "[3] Multiply" << std::endl;
         std::cout << "[4] Divide" << std::endl;
-        std::cout << "[5] Quit" << std::endl;
+        std::cout << "[5] Memory" << std::endl;
+        std::cout << "[6] Quit" << std::endl;
 
         // Get the user input.
         std::string userInput;
         std::cout << "Please enter the number of the option you would like: ";
         std::cin >> userInput;
         // Check the input to see if it is 1.) a number, and 2.) in answer range.
-        if (isNumb(userInput) && inRange(userInput, 1, 5))
+        if (isNumb(userInput) && inRange(userInput, 1, 6))
         {
             // Convert the userInput to an int.
             int problemType = std::stoi(userInput);
 
             // Quit the program.
-            if (problemType == 5)
+            if (problemType == 6)
             {
                 // Break the loop because the user is quiting the program.
                 mainEnd = true;
+            } else if (problemType == 5) { // Check the memory and see what past problems have been done.
+                // Call the secret menu function to take over.
+                secretMenu();
             } else { // Do everything else.
                 // Create type of problem UI for user.
                 std::string problemSelected;
@@ -244,6 +380,9 @@ int main()
             std::cin.get();
         }
     }
+
+    // Things to do before quit, delete the pointers.
+    deletePointers();
 
     // Quiting message.
     std::cout << "Thank you for using the Cool Calculator!" << std::endl;
